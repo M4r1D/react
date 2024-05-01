9ab703951a573797c5cf7ef184038c9ce6984506
Mari <122611455+M4r1D@users.noreply.github.com>
2024-04-30 21:10:54 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Mensagens from './components/Mensagens';
+import MensagensNew from './components/MensagemNew';
+
+function App() {
+  return (
+    <div><Mensagens></Mensagens>
+    <MensagensNew></MensagensNew>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/MensagemNew.js
@@ -0,0 +1,8 @@
+function MensagensNew(){
+    return(
+        <div>
+            <h1>AEEE CALABRESO!!!!!!</h1>
+        </div>
+    );
+}
+export default MensagensNew;
